d237c7aedf45470d292477fac13194b10678fb52
deni-rocha <eu.denilsonrocha@gmail.com>
2022-09-16 14:56:13 -0300

    project base

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
+      <header>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          <Home />
         </a>
       </header>
     </div>
